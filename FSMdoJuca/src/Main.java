142f85e8ce422ce8b7640cf51dfb29af36e708c4
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-09-03 19:39:52 -0300

    created enum and switch statement

--- FSMdoJuca/src/Main.java
+++ FSMdoJuca/src/Main.java
@@ -1,6 +1,34 @@
-public class Main {
+public class Main
+{
+    enum States
+    {
+        WORKING,
+        EATING,
+        SLEEPING
+    }
+
     public static void main(String[] args)
     {
-        
+        States currentState = States.WORKING;
+
+        boolean isLoopActive = true;
+
+        while(isLoopActive)
+        {
+            checkState(currentState);
+        }
+    }
+
+    public static void checkState(States state)
+    {
+        switch (state)
+        {
+            case WORKING:
+                break;
+            case EATING:
+                break;
+            case SLEEPING:
+                break;
+        }
     }
 }
