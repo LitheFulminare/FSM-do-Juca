7a0fc1a4f1cfe4742e1aad09ddce32c72c126a0e
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-09-10 18:46:37 -0300

    increase and decrease stats

--- FSMdoJuca/src/Main.java
+++ FSMdoJuca/src/Main.java
@@ -7,6 +7,9 @@ public class Main
         SLEEPING
     }
 
+    static int hunger = 0;
+    static int fatigue = 0;
+
     public static void main(String[] args)
     {
         States currentState = States.WORKING;
@@ -24,11 +27,35 @@ public class Main
         switch (state)
         {
             case WORKING:
+                work();
                 break;
             case EATING:
+                eat();
                 break;
             case SLEEPING:
+                sleep();
                 break;
         }
+    }
+
+    public static void work()
+    {
+        hunger += 2;
+        fatigue += 5;
+    }
+
+    public static void eat()
+    {
+        hunger -= 5;
+        if (hunger > 0)
+        {
+            hunger = 0;
+        }
+    }
+
+    public static void sleep()
+    {
+        hunger += 1;
+        fatigue -= 10;
     }
 }
